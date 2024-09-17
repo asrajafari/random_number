2406fe0ad39785714d7229165965981d6f50604c
asrajafari <168227642+asrajafari@users.noreply.github.com>
2024-09-17 23:54:48 +0330

    commit all

---
+++ random_number/Program.cs
@@ -0,0 +1,34 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(0, 101);
+
+
+Console.WriteLine("Guess a random number between 1 - 100");
+int yourGuess = int.Parse(Console.ReadLine());
+
+while (yourGuess != randomNumber)
+{
+    Console.WriteLine("Guess a number"+ yourGuess);
+    //int yourGuess = int.Parse(Console.ReadLine());
+
+    if (yourGuess > randomNumber)
+    {
+        Console.WriteLine( "your guess is more");
+        break;
+    }
+    else if (yourGuess < randomNumber)
+    {
+        Console.WriteLine("your guess is less");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("yep.. its true");
+        break;
+    }
+        Console.WriteLine("game over");
+    
+}
+
+
+
+
